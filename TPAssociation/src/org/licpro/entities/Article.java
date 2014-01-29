36fbf436e1041318dd56a0402050e3f25481892b
E115903G <E115903G@U-INF-J-E113-15.iut-nantes.univ-nantes.prive>
2014-01-29 16:23:59 +0100

    Ajout classes entity

---
+++ TPAssociation/src/org/licpro/entities/Article.java
@@ -0,0 +1,88 @@
+package org.licpro.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ARTICLE", schema = "association")
+public class Article {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private int Id;
+
+	@Column(name = "CODE")
+	private String code;
+
+	@Column(name = "NAME")
+	private String nom;
+
+	@Column(name = "PRICE")
+	private double prix;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return Id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		Id = id;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code
+	 *            the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * @param nom
+	 *            the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prix
+	 */
+	public double getPrix() {
+		return prix;
+	}
+
+	/**
+	 * @param prix
+	 *            the prix to set
+	 */
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+
+}
---
+++ TPAssociation/src/org/licpro/entities/Commande.java
@@ -0,0 +1,53 @@
+package org.licpro.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "COMMANDE", schema = "association")
+public class Commande {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+
+	@OneToMany
+	private List<Article> articles;
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return Id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(int id) {
+		Id = id;
+	}
+
+	/**
+	 * @return the articles
+	 */
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	/**
+	 * @param articles
+	 *            the articles to set
+	 */
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+
+}
